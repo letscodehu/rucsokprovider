4a5db732c185c36a195e2972d289e6eace8abf00
Papp Krisztian <Papp_Krisztian@epam.com>
2016-07-08 11:00:25 +0200

    layout modified, footer position fix

--- src/main/resources/static/js/controller/rucsokController.js
+++ src/main/resources/static/js/controller/rucsokController.js
@@ -1,57 +1,72 @@
 define(
 		[
 		 "jquery"
-        ], function($) {
-			
-	rucsokController.$inject = ["rucsokService"]		
-			
-	function rucsokController(rucsokService) {
-		var vm = this;
+		 ], function($) {
 
-		vm.rucsoks = [];
-		
-		vm.dirty = true;
-		vm.preload = false;
-		vm.addRucsok = addRucsok;
-		vm.checkRucsok = checkRucsok;
-		vm.showAddModal = showAddModal;
-		vm.rucsok = rucsok;
-		
-		function addRucsok() {
-			vm.preload = true;
-			rucsokService.addRucsok(vm.rucsok).then(function(){
+			rucsokController.$inject = ["rucsokService"]		
+
+			function rucsokController(rucsokService) {
+				var vm = this;
+
+				vm.rucsoks = [];
+
+				vm.dirty = true;
 				vm.preload = false;
-				rucsokService.getRucsok().then(function(data) {
-					vm.rucsoks = data;
-					closeAddModal();
-				});	
-			})
-		}
-		
-		function checkRucsok() {
-			rucsokService.checkRucsok(vm.url).then(function(data) {
-				vm.dirty = false;
-				vm.rucsok = data;
-			});
-		}		
-		
-		function showAddModal() {
-			$("#rucsok-modal").modal("show");
-		}
-		
-		function closeAddModal() {
-			$("#rucsok-modal").modal("hide");
-		}
-		
-		function rucsok() {
-			rucsokService.getRucsok().then(function(data) {
-				vm.rucsoks = data;
-				if (vm.rucsoks.length == 0) {
-					showAddModal();
+				vm.addRucsok = addRucsok;
+				vm.checkRucsok = checkRucsok;
+				vm.visit = visit;
+				vm.showAddModal = showAddModal;
+				vm.rucsok = rucsok;
+
+				vm.refresh = refresh;
+
+				function refresh() {
+					rucsokService.getRucsok().then(function(data) {
+						vm.rucsoks = data;
+					});
 				}
-			});
-		}
-	}
-	
-	return rucsokController;
-})
+				
+				
+				function visit(url) {
+					  var win = window.open(url, '_blank');
+					  win.focus();
+				}
+
+				function addRucsok() {
+					vm.preload = true;
+					rucsokService.addRucsok(vm.rucsok).then(function(){
+						vm.preload = false;
+						rucsokService.getRucsok().then(function(data) {
+							vm.rucsoks = data;
+							closeAddModal();
+						});	
+					})
+				}
+
+				function checkRucsok() {
+					rucsokService.checkRucsok(vm.url).then(function(data) {
+						vm.dirty = false;
+						vm.rucsok = data;
+					});
+				}		
+
+				function showAddModal() {
+					$("#rucsok-modal").modal("show");
+				}
+
+				function closeAddModal() {
+					$("#rucsok-modal").modal("hide");
+				}
+
+				function rucsok() {
+					rucsokService.getRucsok().then(function(data) {
+						vm.rucsoks = data;
+						if (vm.rucsoks.length == 0) {
+							showAddModal();
+						}
+					});
+				}
+			}
+
+			return rucsokController;
+		})
