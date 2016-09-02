a01912ea8601b4e5ddf6c4054896f52466b065a1
David_Hajnal <David_Hajnal@epam.com>
2016-09-02 13:19:16 +0200

    removed static content from service layer

--- rucsok-service/src/main/resources/public/app/controllers/profile/profileUpvoteController.js
+++
@@ -1,9 +0,0 @@
-define([], function() {
-
-	function errorController() {
-		var vm = this;
-		vm.errorImage = "https://i.imgur.com/QHvQ0tF.gif";
-	}
-
-	return errorController;
-})
--- rucsok-service/src/main/resources/static/js/controller/errorController.js
+++
@@ -1,9 +0,0 @@
-define([ "jquery" ], function($) {
-
-	function errorController() {
-		var vm = this;
-		vm.errorImage = "https://i.imgur.com/QHvQ0tF.gif";
-	}
-
-	return errorController;
-})
